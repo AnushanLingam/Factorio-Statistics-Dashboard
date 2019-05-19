aa37aabd9550e33f29d4e8d1a400cf42da65cb8c
Anushan Lingam <5958017+AnushanLingam@users.noreply.github.com>
2019-05-19 13:13:36 +0100

    moved to factorio api table_to_json method to collect data

--- mod/StatsDashboard_0.1.0/control.lua
+++ mod/StatsDashboard_0.1.0/control.lua
@@ -1,63 +1,58 @@
 script.on_event(
     {defines.events.on_tick},
     function(e)
+        local player = game.get_player(1)
 
-        if e.tick % 30 == 0 then -- adjust tick at the end to ensure no game lag
-            local player = game.get_player(1)
-            local prodStats = player.force.item_production_statistics.output_counts
-            local fluidStats = player.force.fluid_production_statistics.output_counts
+        -- Collects data at alternating intervals to avoid performance drop
+        if e.tick % 30 == 0 then
+            -- Collect output statistics
+            local itemOutput = game.table_to_json(player.force.item_production_statistics.output_counts)
+            local fluidOutput = game.table_to_json(player.force.fluid_production_statistics.output_counts)
 
-            -- Writes data to text file using built in logging tool
-            -- Conversion to json format happens on web server to alleviate performance issues during game
-            game.write_file("itemOutputStats.txt", serpent.block(prodStats))
-            game.write_file("fluidOutputStats.txt", serpent.block(fluidStats))
+            -- Write outputs to file
+            game.write_file("statsDashboard/output.json", "[ \n")
+            game.write_file("statsDashboard/output.json", string.format("%s, \n %s \n", itemOutput, fluidOutput), true)
+            game.write_file("statsDashboard/output.json", "] \n", true)
+        end
+        if e.tick % 45 == 0 then
+            -- Collect input statistics
+            local itemInput = game.table_to_json(player.force.item_production_statistics.input_counts)
+            local fluidInput = game.table_to_json(player.force.fluid_production_statistics.input_counts)
 
-
-            -- NOTE 
-            -- Following code converts to json in-game which causes heavy performance drops at 30 ticks.
-            -- Increasing the duration leads to a framerate spike at the set interval so not recommended 
-            -- -- collect output stats
-            -- game.write_file("outputs.json", "{ \n")
-            -- tableToJson("itemProduction", prodStats, true, "outputs.json")
-            -- tableToJson("fluidProduction", fluidStats, false, "outputs.json")
-            -- game.write_file("outputs.json", "}", true)
-
-            -- -- collect input stats
-            -- local prodInputs = player.force.item_production_statistics.input_counts
-            -- local fluidInputs = player.force.fluid_production_statistics.input_counts
-            -- game.write_file("inputs.json", "{ \n")
-            -- tableToJson("itemInputs", prodInputs, true, "inputs.json")
-            -- tableToJson("fluidInputs", fluidInputs, false, "inputs.json")
-            -- game.write_file("inputs.json", "}", true)
+            -- Write inputs to file
+            game.write_file("statsDashboard/input.json", "[ \n")
+            game.write_file("statsDashboard/input.json", string.format("%s, \n %s \n", itemInput, fluidInput), true)
+            game.write_file("statsDashboard/input.json", "] \n", true)
         end
     end
 )
 
-function tableToJson(name, dataTable, shouldComma, fileName)
-    game.write_file(fileName, string.format('"%s": [ \n', name), true)
-    -- count number of keys in stats table
-    local count = 0
-    for _ in pairs(dataTable) do
-        count = count + 1
-    end
+-- NOTE - MOVED TO BUILT IN TABLE_TO_JSON METHOD
+-- function tableToJson(name, dataTable, shouldComma, fileName)
+--     game.write_file(fileName, string.format('"%s": [ \n', name), true)
+--     -- count number of keys in stats table
+--     local count = 0
+--     for _ in pairs(dataTable) do
+--         count = count + 1
+--     end
 
-    -- Loop through each key and convert to string
-    local keyset = {}
-    local n = 0
-    for k, v in pairs(dataTable) do
-        n = n + 1
-        keyset[n] = k
-        local comma = ","
-        if count == n then
-            comma = ""
-        end
-        local dataString = string.format('{\n "name": "%s", \n"value": %s \n}%s\n', k, tostring(v), comma)
-        game.write_file(fileName, dataString, true)
-    end
+--     -- Loop through each key and convert to string
+--     local keyset = {}
+--     local n = 0
+--     for k, v in pairs(dataTable) do
+--         n = n + 1
+--         keyset[n] = k
+--         local comma = ","
+--         if count == n then
+--             comma = ""
+--         end
+--         local dataString = string.format('{\n "name": "%s", \n"value": %s \n}%s\n', k, tostring(v), comma)
+--         game.write_file(fileName, dataString, true)
+--     end
 
-    local endComma = ","
-    if shouldComma == false then
-        endComma = ""
-    end
-    game.write_file(fileName, string.format("]%s \n", endComma), true)
-end
+--     local endComma = ","
+--     if shouldComma == false then
+--         endComma = ""
+--     end
+--     game.write_file(fileName, string.format("]%s \n", endComma), true)
+-- end
